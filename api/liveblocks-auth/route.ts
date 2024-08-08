d05c91d7d3bee747cf1437adefc27a3e3b23ceda
naman rathore <namanrathore59@gmail.com>
2024-08-08 09:01:11 +0000

    error enconter

--- api/liveblocks-auth/route.ts
+++ api/liveblocks-auth/route.ts
@@ -5,32 +5,32 @@ import { redirect } from "next/navigation";
 
 
 export async function POST(request: Request) {
-  // Get the current user from your database
-  const clerkUser=await currentUser()
+  const clerkUser = await currentUser();
 
-  if(!clerkUser)redirect('/sign-in')
-  const {id ,firsName,lastName,emailAddress,imageUrl} = clerkUser;
+  if(!clerkUser) redirect('/sign-in')
+  const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
 
-    const user ={
-        id,
-        info:{
-            id:,
-            name:`${firsName} ${lastName}`,
-            email:emailAddress[0].emailAddress,
-            avatar:imageUrl,
-            color:getUserColor(id)
-        }
+  const user = {
+    id,
+    info: {
+      id,
+      name: `${firstName} ${lastName}`,
+      email: emailAddresses[0].emailAddress,
+      avatar: imageUrl,
+      color: getUserColor(id)
     }
+  }
 
-  const user = __getUserFromDB__(request);
+  // const user = __getUserFromDB__(request);
 
   // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
+
       userId: user.id,
-      groupIds, // Optional
+      groupIds: [], 
     },
-    { userInfo: user.metadata },
+    { userInfo: user.info },
   );
 
   return new Response(body, { status });
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+
+import AddDocbtn from "@/components/AddDocbtn";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const clerkUser = await currentUser()
+  if (!clerkUser) redirect("/sign-in")
+  const document = []
+
   return (
-   
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {
+        document.length > 0 ? (
+          <div></div>
+        ) : (
+          <div className="document-list-empty">
+            <Image src="assets/icons/doc.svg" alt="document icon" width={40} height={40} className="mx-auto" />
+            <AddDocbtn 
+            userId={clerkUser.id}
+             email={clerkUser.emailAddresses[0].emailAddress} 
+             />
+          </div>
+        )
+      }
+    </main>
+  )
 }
+
+export default Home
